c62131c6708a7506ac2f9ee28e6caffb02197675
FNeal10 <altares.fneal@gmail.com>
2025-04-27 21:15:22 -0400

    Added Silver

--- utils/text_cleaning.py
+++ utils/text_cleaning.py
@@ -1,3 +1,8 @@
 def basic_text_cleaning(text):
-    text = text.replace("\n", " ").replace("\\n", " ").replace("\r", " ").replace("\\", " ").strip()
-    return text
+    text = text.replace("\n", " ").replace("\\n", " ").replace("\r", " ").replace("\\", " ")
+    
+    text = text.replace(","," ").replace("&", "and").replace('""', '')
+    
+    text = text.lower()
+    
+    return text.strip()
